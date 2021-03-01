ad4d876d350a6b2ec5eee60a6ae2c1f9e644c23d
Abdul Qadeer Solangi <abdulqadeersolangi007@gmail.com>
2021-03-01 21:16:57 +0500

    simple cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
+
 function App() {
+
+  const [value, setValue] = useState(0)
+
+  function add() {
+    setValue(value + 1)
+  }
+  function sub() {
+    setValue(value - 1)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className="cart1">
+        
+
+        <br />
+        <br />
+        
+        <button  style = {{marginRight : "40px"}}  onClick={sub}  > - </button>
+        {value}
+        <button style = {{marginRight : "40px" }} onClick={add} > + </button>
+        <br />
+        <br />
+        <div className="cart2">
+          <img width="250px" src="https://u5j4h3u7.stackpathcdn.com/pub/media/catalog/product/cache/172d96e30d7cd3d4380b53391edef300/3/0/303395.jpg" alt="sweets" /> <br />
+          {/* <button style={{ marginRight: "20px" }} onClick={add}>Add to cart</button> */}
+   
+          <button type="button" class="btn btn-outline-success" onClick = {add}>Add To Cart</button>
+
+        </div>
+      </div>
     </div>
+
+
   );
 }
 
