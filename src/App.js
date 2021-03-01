fbac1e9a2c39829ac5e1020f5331f11e079c85d0
Abdul Qadeer Solangi <abdulqadeersolangi007@gmail.com>
2021-03-01 21:48:41 +0500

    cart using state

--- src/App.js
+++ src/App.js
@@ -24,14 +24,18 @@ function App() {
         <br />
         <br />
         
-        <button  style = {{marginRight : "40px"}}  onClick={sub}  > - </button>
+       
+        <button type="button" class="btn btn-danger"onClick = {sub} > - </button>
+        <br />
         {value}
-        <button style = {{marginRight : "40px" }} onClick={add} > + </button>
+        <br />
+       
+        <button type="button" class="btn btn-primary" onClick= {add} > + </button>
         <br />
         <br />
         <div className="cart2">
           <img width="250px" src="https://u5j4h3u7.stackpathcdn.com/pub/media/catalog/product/cache/172d96e30d7cd3d4380b53391edef300/3/0/303395.jpg" alt="sweets" /> <br />
-          {/* <button style={{ marginRight: "20px" }} onClick={add}>Add to cart</button> */}
+         
    
           <button type="button" class="btn btn-outline-success" onClick = {add}>Add To Cart</button>
 
