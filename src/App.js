d7c3ee7ead62a5fde7ef2f1c803a78b4272a4b2a
Abdul Qadeer Solangi <abdulqadeersolangi007@gmail.com>
2021-03-03 01:12:57 +0500

    condition on button not working

--- src/App.js
+++ src/App.js
@@ -5,44 +5,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react'
 
 function App() {
-
+  const [value , setValue] = useState(1)
   const [cart, setCart] = useState([])
   const [product, setproduct] = useState([
     {
-      title: "sweet",
+      title: "sweet1",
       price: "200",
+      id : "123",
       imgaeUrl: "https://u5j4h3u7.stackpathcdn.com/pub/media/catalog/product/cache/172d96e30d7cd3d4380b53391edef300/3/0/303395.jpg"
     },
     {
       title: 'sweet2',
       price: "100",
+      id : "234",
       imgaeUrl: "https://u5j4h3u7.stackpathcdn.com/pub/media/catalog/product/cache/172d96e30d7cd3d4380b53391edef300/3/0/303395.jpg"
     },
     {
       title: "sweet3",
       price: "50",
+      id : "789",
       imgaeUrl: "https://u5j4h3u7.stackpathcdn.com/pub/media/catalog/product/cache/172d96e30d7cd3d4380b53391edef300/3/0/303395.jpg "
     }
   ])
 
 
 
-  // function add() {
-  //   setValue(value + 1)
-  // }
-  // function sub() {
-  //   setValue(value - 1)
+  function add() {
+    setValue(value + 1)
+  }
+  function sub() {
 
-  // }
+    setValue(value)
+  if( value.visible === 1){
+    setValue({value : "block"})
+  }
+  }
 
   function addcart(index) {
-var i = {
-  price : product[index].price,
+var add = {
   tittle : product[index].title,
-  imgaeUrl : product[index].imgaeUrl
+  price : product[index].price,
+  id : product[index].id,
+  imgaeUrl : product[index].imgaeUrl,
   
 }
-  setCart([...cart,i])
+  setCart([...cart,add])
   console.log(cart)
 
   }
@@ -56,6 +63,7 @@ var i = {
         <div key={index}>
           <h5>{value.title}</h5>
           <h5>{value.price} </h5>
+          <h5>{value.id}</h5>
           <img src={value.imgaeUrl} className="cart2" alt="sweet" />
 
           <br />
@@ -68,10 +76,14 @@ var i = {
         </div>
       ))}
        <div>
-      { cart.map((value, index) => (
+      { cart.map((event, index) => (
         <div key={index}>
-          <h5>{value.title}</h5>
-          <h5>{value.price} </h5>
+          <h5>{event.title}</h5>
+          <h3> price = {event.price} </h3>
+          <h5> id = {event.id}</h5>
+          <button onClick = {add}  >+</button> {value}
+          <button onClick = {sub}  >-</button> 
+         
          
 
        </div>
